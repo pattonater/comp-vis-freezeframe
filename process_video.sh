02f57b202cd040e9c154356881f5d9d3ef322fee
Ryan Patton <ryan.patton@dycominc.com>
2017-05-09 09:07:17 +0200

    video script updated to clean up any preexisting files

--- process_video.sh
+++ process_video.sh
@@ -17,28 +17,43 @@ make
 popd
 
 # initialize variable names
-outputVideoName="tracked.mp4"
+outputVideo="output/tracked.mp4"
 inputFolder="input/tmp_stills"
 imageBaseName="img_still"
 outputFolder="output/tmp_stills"
 fps=24
 # these must already exist
-video="input/screen_corners.m4v"
+inputVideo="input/screen_corners.m4v"
 markersFolder="input/markers"
 markerBaseName="marker"
+
+# remove any files left over from previous runs
+if test -d $inputFolder; then 
+    rm -r $inputFolder
+fi
+if  test -d $outputFolder; then 
+    rm -r $outputFolder
+fi
+if test -e $outputVideo; then
+    rm $outputVideo
+fi
 
 # make folders and fill input folder with image stills from video
 mkdir $inputFolder
 mkdir $outputFolder
-ffmpeg -i $video -vf fps=$fps $inputFolder/$imageBaseName%d.jpg
+ffmpeg -i $inputVideo -vf fps=$fps $inputFolder/$imageBaseName%d.jpg
 
 # run harryPotterize on folder
 src/imgpro $inputFolder $imageBaseName $outputFolder \
     -harryPotterize $markersFolder $markerBaseName
 
 # make video out of output images
-ffmpeg -framerate $fps -i $outputFolder/$imageBaseName%d.jpg output/$outputVideoName
+ffmpeg -framerate $fps -i $outputFolder/$imageBaseName%d.jpg $outputVideo
 
 # erase image stills
 rm -r $inputFolder
 rm -r $outputFolder
+
+
+
+
